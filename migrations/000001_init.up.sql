4e440af7662e8ce90fee826c30108eaa1623de6e
Evgenii <e.khovalyg@innopolis.university>
2026-06-21 08:15:40 +0300

    infra setup

---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,30 @@
+CREATE SCHEMA todoapp;
+
+CREATE TABLE todoapp.users (
+    id           INTEGER      GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    version      BIGINT       NOT NULL DEFAULT 1,
+    full_name    VARCHAR(100) NOT NULL CHECK (char_length(full_name) BETWEEN 3 AND 100),
+    phone_number VARCHAR(15)  CHECK (
+                                phone_number ~ '^\+[0-9]+$' 
+                                AND 
+                                char_length(phone_number) BETWEEN 10 AND 15
+                              )
+);
+
+
+CREATE TABLE todoapp.tasks (
+    id             INTEGER       GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    version        BIGINT        NOT NULL DEFAULT 1,
+    title          VARCHAR(100)  NOT NULL CHECK (char_length(title) BETWEEN 1 AND 100),
+    description    VARCHAR(1000) CHECK (char_length(description) BETWEEN 1 AND 1000),
+    completed      BOOLEAN       NOT NULL,
+    create_at      TIMESTAMPTZ   NOT NULL,
+    completed_at   TIMESTAMPTZ,
+    author_user_id INTEGER       NOT NULL REFERENCES todoapp.users(id),
+
+    CHECK (
+        (completed=FALSE AND completed_at IS NULL) 
+        AND
+        (completed=TRUE AND completed_at IS NOT NULL AND completed_at >= create_at)
+    )    
+);
